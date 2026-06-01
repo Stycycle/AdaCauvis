a9f829cc4299eb26a532cfa8b7887089153bd340
Stycycle <88068759@qq.com>
2026-06-01 17:04:53 +0800

    Add baseline config and test script for original DINOv2

---
+++ configs/cauvis/baseline_dinov2_dinohead_bs1x4_sdgod.py
@@ -0,0 +1,53 @@
+# Baseline config: Original DINOv2 without Cauvis adaptive layer
+_base_ = [
+    "../_base_/datasets/Sdgod.py",
+    "../_base_/default_runtime.py",
+    "../_base_/models/dinov2_dinohead.py"
+]
+
+img_scales = (640, 640)
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=img_scales, keep_ratio=False),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+train_per_gpu_batch_size = 2
+train_dataloader = dict(batch_size=train_per_gpu_batch_size,
+                        dataset=dict(filter_cfg=dict(filter_empty_gt=False), pipeline=train_pipeline))
+classes = 7
+
+# Override num_classes for SDGOD dataset
+model = dict(bbox_head=dict(num_classes=classes))
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW',
+        lr=0.0001,
+        weight_decay=0.0001),
+    clip_grad=dict(max_norm=0.1, norm_type=2),
+    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)})
+)
+
+# learning policy
+max_epochs = 4
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=12)
+
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[11],
+        gamma=0.1)
+]
+
+auto_scale_lr = dict(base_batch_size=16)
